0e357be2504023b3d13bc9d67033f5d7dab54123
zetxx <opensuser@gmail.com>
2023-03-15 16:50:10 +0200

    fix: query time

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const digz = require('digz');
+const {query} = require('digz');
 const Koa = require('koa');
 const Router = require('@koa/router');
 const {listen} = require('rc')(require('./package.json').name, {listen: 3000});
@@ -8,17 +8,18 @@ const router = new Router();
 
 router.get('/:host/:port/:wait', async(ctx, next) => {
     const {params: {host, port, wait}} = ctx;
-    const d = new Date();
-    const waitTime = (((wait > 60) && wait) || 60);
-    const timestr = [d.getHours(), d.getMinutes()].join(':');
+    const waitTime = (isNaN(wait) && 60) || parseInt(wait);
     console.info(`will query ${host}:${port} after ${waitTime} sec.`)
     await new Promise((res, rej) => {
         setTimeout(async() => {
-            const s = await digz.query({
+            const d = new Date();
+            const timestr = [d.getHours(), d.getMinutes()].join(':');
+            const s = await query({
                 host,
                 port
             });
             ctx.body = [timestr, s.players_connected].join('| ');
+            console.info(s);
             res();
         }, waitTime * 1000);
     });
